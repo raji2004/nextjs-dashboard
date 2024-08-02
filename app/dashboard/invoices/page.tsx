4f00961adf9dda87ca5442a881468252b2436fd1
raji <kingraj1344@gmail.com>
2024-08-02 19:23:24 +0100

    chore: Add lazy loading for images, update metadata, and improve authentication handling

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,6 +7,11 @@ import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
  
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 export default async function Page({searchParams,}:{searchParams:{query?: string,page?: string}}) {
   const query = searchParams.query ?? '';
   const currentPage = Number(searchParams.page) || 1;
